80079c92c01e5ace1fdfc11073f1b007b1d0e0fa
Glank-Work <glank314@gmail.com>
2013-08-05 12:01:44 -0400

    Completed up to 'Use generic views...' in tutorial04

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,5 +16,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     #index
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
 )
